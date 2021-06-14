0e0ad91e2c4f873ed22cb03d5246251ce040eb14
Nagarjuna Chaganti <nchagant@codeaurora.org>
2021-06-14 17:36:52 +0530

    net: stmmac: Fix type casting for tlmm addr
    Fix type casting for tlmm addr.
    
    Change-Id: I3244ab394db6fef3d78cba6c17ffb9a12534d4cb
    Signed-off-by: Nagarjuna Chaganti <nchagant@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -34,7 +34,7 @@
 /* Customer can use this path to configure mac address */
 #define MAC_ADDR_CFG_FPATH "/etc/data/emac_config.ini"
 
-static unsigned long tlmm_central_base_addr;
+static void __iomem *tlmm_central_base_addr;
 bool phy_intr_en;
 
 static char err_names[10][14] = {"PHY_RW_ERR",
@@ -1746,9 +1746,9 @@ static int ethqos_update_rgmii_tx_drv_strength(struct qcom_ethqos *ethqos)
 	ETHQOSDBG("tlmm_central_base = 0x%x, size = 0x%x\n",
 		  tlmm_central_base, tlmm_central_size);
 
-	tlmm_central_base_addr = (unsigned long)ioremap(
+	tlmm_central_base_addr = ioremap(
 	   tlmm_central_base, tlmm_central_size);
-	if ((void __iomem *)!tlmm_central_base_addr) {
+	if (!tlmm_central_base_addr) {
 		ETHQOSERR("cannot map dwc_tlmm_central reg memory, aborting\n");
 		ret = -EIO;
 		goto err_out;
@@ -1788,7 +1788,7 @@ static int ethqos_update_rgmii_tx_drv_strength(struct qcom_ethqos *ethqos)
 
 err_out:
 	if (tlmm_central_base_addr)
-		iounmap((void __iomem *)tlmm_central_base_addr);
+		iounmap(tlmm_central_base_addr);
 
 	return ret;
 }
