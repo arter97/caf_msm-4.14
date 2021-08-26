62044bb830923aeb3ef8db5a81ab070e147c5168
Ilia Lin <ilialin@codeaurora.org>
2021-08-26 11:14:33 +0300

    ipa: Null persistent pointers after free
    Assign NULL to pointers that may be used later
    after calling kfree on them.
    
    Change-Id: I3298eb484c92ee2373f0bc41aae8ae45fb373cf0
    Signed-off-by: Ilia Lin <ilialin@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_uc_wdi.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_uc_wdi.c
@@ -750,6 +750,7 @@ static void ipa_release_ap_smmu_mappings(enum ipa_client_type client)
 				ipa3_ctx->wdi_map_cnt--;
 			}
 			kfree(wdi_res[i].res);
+			wdi_res[i].res = NULL;
 			wdi_res[i].valid = false;
 		}
 	}
@@ -786,6 +787,7 @@ static void ipa_release_uc_smmu_mappings(enum ipa_client_type client)
 				ipa3_ctx->wdi_map_cnt--;
 			}
 			kfree(wdi_res[i].res);
+			wdi_res[i].res = NULL;
 			wdi_res[i].valid = false;
 		}
 	}
@@ -937,6 +939,7 @@ void ipa3_release_wdi3_gsi_smmu_mappings(u8 dir)
 				ipa3_ctx->wdi_map_cnt--;
 			}
 			kfree(wdi_res[i].res);
+			wdi_res[i].res = NULL;
 			wdi_res[i].valid = false;
 		}
 	}
