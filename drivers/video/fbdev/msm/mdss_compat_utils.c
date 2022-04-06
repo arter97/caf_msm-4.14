ec351d57ec73e707a62776e9355f18b7703ad3fb
xiangxuy <quic_xiangxuy@quicinc.com>
2022-04-06 15:10:20 +0800

    disp: msm: fbdev revert snapshot of mdss driver
    Revert I6d4aabb5f2f156c701209352f3364999bb91fd60 and
    subsequent related changes.
    With this change dsi should crash during probe and hdmi vsync can't
    work.
    
    Change-Id: Ic170f46a12168e3ab4d3436b2152eb6d0f74bc4a
    Signed-off-by: xiangxuy <quic_xiangxuy@quicinc.com>

--- drivers/video/fbdev/msm/mdss_compat_utils.c
+++ drivers/video/fbdev/msm/mdss_compat_utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018, 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 1994 Martin Schaller
  *
  * 2001 - Documented with DocBook
@@ -119,9 +119,10 @@ static unsigned int __do_compat_ioctl_nr(unsigned int cmd32)
 static void  __copy_atomic_commit_struct(struct mdp_layer_commit  *commit,
 	struct mdp_layer_commit32 *commit32)
 {
-	unsigned int destSize = sizeof(commit->commit_v1.reserved);
-	unsigned int srcSize = sizeof(commit32->commit_v1.reserved);
-	unsigned int count = (destSize <= srcSize ? destSize : srcSize);
+	unsigned int destsize = sizeof(commit->commit_v1.reserved);
+	unsigned int srcsize = sizeof(commit32->commit_v1.reserved);
+	unsigned int count = (destsize <= srcsize ? destsize : srcsize);
+
 	commit->version = commit32->version;
 	commit->commit_v1.flags = commit32->commit_v1.flags;
 	commit->commit_v1.input_layer_cnt =
@@ -220,7 +221,6 @@ static struct mdp_input_layer *__create_layer_list(
 
 		layer->flags = layer32->flags;
 		layer->pipe_ndx = layer32->pipe_ndx;
-		layer->rect_num = layer32->rect_num;
 		layer->horz_deci = layer32->horz_deci;
 		layer->vert_deci = layer32->vert_deci;
 		layer->z_order = layer32->z_order;
@@ -306,6 +306,7 @@ static int __compat_atomic_commit(struct fb_info *info, unsigned int cmd,
 	struct mdp_input_layer *layer_list = NULL;
 	struct mdp_input_layer32 *layer_list32 = NULL;
 	struct mdp_output_layer *output_layer = NULL;
+	struct mdp_frc_info *frc_info = NULL;
 
 	/* copy top level memory from 32 bit structure to kernel memory */
 	ret = copy_from_user(&commit32, (void __user *)argp,
@@ -322,9 +323,11 @@ static int __compat_atomic_commit(struct fb_info *info, unsigned int cmd,
 
 	if (commit32.commit_v1.output_layer) {
 		int buffer_size = sizeof(struct mdp_output_layer);
+
 		output_layer = kzalloc(buffer_size, GFP_KERNEL);
 		if (!output_layer)
 			return -ENOMEM;
+
 		ret = copy_from_user(output_layer,
 				compat_ptr(commit32.commit_v1.output_layer),
 				buffer_size);
@@ -365,6 +368,29 @@ static int __compat_atomic_commit(struct fb_info *info, unsigned int cmd,
 		}
 	}
 
+	if (commit32.commit_v1.frc_info) {
+		int buffer_size = sizeof(struct mdp_frc_info);
+
+		frc_info = kzalloc(buffer_size, GFP_KERNEL);
+		if (!frc_info) {
+			ret = -ENOMEM;
+			goto frc_err;
+		}
+
+		ret = copy_from_user(frc_info,
+				compat_ptr(commit32.commit_v1.frc_info),
+				buffer_size);
+		if (ret) {
+			pr_err("fail to copy frc info from user, ptr %p\n",
+				compat_ptr(commit32.commit_v1.frc_info));
+			kfree(frc_info);
+			ret = -EFAULT;
+			goto frc_err;
+		}
+
+		commit.commit_v1.frc_info = frc_info;
+	}
+
 	ret = mdss_fb_atomic_commit(info, &commit, file);
 	if (ret)
 		pr_err("atomic commit failed ret:%d\n", ret);
@@ -377,6 +403,9 @@ static int __compat_atomic_commit(struct fb_info *info, unsigned int cmd,
 		kfree(layer_list[i].scale);
 		mdss_mdp_free_layer_pp_info(&layer_list[i]);
 	}
+
+	kfree(frc_info);
+frc_err:
 	kfree(layer_list);
 layer_list_err:
 	kfree(layer_list32);
@@ -517,9 +546,8 @@ static int mdss_fb_compat_buf_sync(struct fb_info *info, unsigned int cmd,
 			sizeof(int))) {
 		if (buf_sync->flags & MDP_BUF_SYNC_FLAG_RETIRE_FENCE)
 			return -EFAULT;
-		else
-			pr_debug("%s: no retire fence fd for wb\n",
-				__func__);
+		pr_debug("%s: no retire fence fd for wb\n",
+			__func__);
 	}
 
 	return ret;
@@ -1263,6 +1291,7 @@ static int __from_user_pgc_lut_data_v1_7(
 {
 	struct mdp_pgc_lut_data_v1_7_32 pgc_cfg_payload_32;
 	struct mdp_pgc_lut_data_v1_7 pgc_cfg_payload;
+
 	if (copy_from_user(&pgc_cfg_payload_32,
 			   compat_ptr(pgc_lut32->cfg_payload),
 			   sizeof(pgc_cfg_payload_32))) {
@@ -2803,6 +2832,7 @@ static u32 __pp_compat_size_hist_lut(void)
 static u32 __pp_compat_size_pgc(void)
 {
 	u32 tbl_sz_max = 0;
+
 	tbl_sz_max =  3 * GC_LUT_SEGMENTS * sizeof(struct mdp_ar_gc_lut_data);
 	tbl_sz_max += sizeof(struct mdp_pgc_lut_data_v1_7);
 	return tbl_sz_max;
--- drivers/video/fbdev/msm/mdss_util.c
+++ drivers/video/fbdev/msm/mdss_util.c
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2007-2018, 2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2007-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -214,6 +214,24 @@ void mdss_disable_irq_wake(struct mdss_hw *hw)
 		pr_warn("MDSS HW ndx=%d is NOT set\n", hw->hw_ndx);
 }
 
+static bool check_display(char *param_string)
+{
+	char *str = NULL;
+	bool display_disable = false;
+
+	str = strnstr(param_string, ";", MDSS_MAX_PANEL_LEN);
+	if (!str)
+		return display_disable;
+
+	str = strnstr(str, ":", MDSS_MAX_PANEL_LEN);
+	if (!str)
+		return display_disable;
+	else if (str[1] == '1')
+		display_disable = 1;
+
+	return display_disable;
+}
+
 struct mdss_util_intf mdss_util = {
 	.register_irq = mdss_register_irq,
 	.enable_irq = mdss_enable_irq,
@@ -229,7 +247,9 @@ struct mdss_util_intf mdss_util = {
 	.bus_scale_set_quota = NULL,
 	.panel_intf_type = NULL,
 	.panel_intf_status = NULL,
-	.mdp_probe_done = false
+	.mdp_probe_done = false,
+	.param_check = check_display,
+	.display_disabled = false
 };
 
 struct mdss_util_intf *mdss_get_util_intf()
