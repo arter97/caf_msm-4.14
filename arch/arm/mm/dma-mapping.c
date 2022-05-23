be180c0ffba5bd0ba043517052e0ed4c9ef557b9
Zhenhua Huang <quic_zhenhuah@quicinc.com>
2022-05-23 11:52:02 +0800

    iommu: dma-mapping-fast: do CMO in a correct way
    In the case of ARM32, page_address() doesn't work for HIGHMEM pages
    if without doing kmap. To fix this, switch to default arm codes to
    do CMO using dma_cache_maint_page. The implementaion does kmap before
    calling page_address().
    
    Change-Id: I3819b3388ec75911749bf0f3d565c9dec34bd21b
    Signed-off-by: Zhenhua Huang <quic_zhenhuah@quicinc.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -111,10 +111,6 @@ static struct arm_dma_buffer *arm_dma_buffer_find(void *virt)
  * before transfers and delay cache invalidation until transfer completion.
  *
  */
-static void __dma_page_cpu_to_dev(struct page *, unsigned long,
-		size_t, enum dma_data_direction);
-static void __dma_page_dev_to_cpu(struct page *, unsigned long,
-		size_t, enum dma_data_direction);
 
 static void *
 __dma_alloc_remap(struct page *page, size_t size, gfp_t gfp, pgprot_t prot,
@@ -1122,7 +1118,7 @@ static void dma_cache_maint_page(struct page *page, unsigned long offset,
  * platforms with CONFIG_DMABOUNCE.
  * Use the driver DMA support - see dma-mapping.h (dma_sync_*)
  */
-static void __dma_page_cpu_to_dev(struct page *page, unsigned long off,
+void __dma_page_cpu_to_dev(struct page *page, unsigned long off,
 	size_t size, enum dma_data_direction dir)
 {
 	phys_addr_t paddr;
@@ -1138,7 +1134,7 @@ static void __dma_page_cpu_to_dev(struct page *page, unsigned long off,
 	/* FIXME: non-speculating: flush on bidirectional mappings? */
 }
 
-static void __dma_page_dev_to_cpu(struct page *page, unsigned long off,
+void __dma_page_dev_to_cpu(struct page *page, unsigned long off,
 	size_t size, enum dma_data_direction dir)
 {
 	phys_addr_t paddr = page_to_phys(page) + off;
