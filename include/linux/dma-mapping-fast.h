7dbadd1991e0d6d752fabb69314ccf5dd556cd4b
Zhenhua Huang <quic_zhenhuah@quicinc.com>
2022-05-23 11:52:02 +0800

    iommu: dma-mapping-fast: do CMO in a correct way
    In the case of ARM32, page_address() doesn't work for HIGHMEM pages
    if without doing kmap. To fix this, switch to default arm codes to
    do CMO using dma_cache_maint_page. The implementaion does kmap before
    calling page_address().
    
    Change-Id: I3819b3388ec75911749bf0f3d565c9dec34bd21b
    Signed-off-by: Zhenhua Huang <quic_zhenhuah@quicinc.com>

--- include/linux/dma-mapping-fast.h
+++ include/linux/dma-mapping-fast.h
@@ -15,6 +15,7 @@
 
 #include <linux/iommu.h>
 #include <linux/io-pgtable-fast.h>
+#include <linux/dma-direction.h>
 
 struct dma_iommu_mapping;
 struct io_pgtable_ops;
@@ -41,6 +42,13 @@ struct dma_fast_smmu_mapping {
 	spinlock_t	lock;
 	struct notifier_block notifier;
 };
+
+#ifndef CONFIG_ARM64
+void __dma_page_cpu_to_dev(struct page *page, unsigned long off,
+			size_t size, enum dma_data_direction dir);
+void __dma_page_dev_to_cpu(struct page *page, unsigned long off,
+			size_t size, enum dma_data_direction dir);
+#endif
 
 #ifdef CONFIG_IOMMU_IO_PGTABLE_FAST
 int fast_smmu_init_mapping(struct device *dev,
