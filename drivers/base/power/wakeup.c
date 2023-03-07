0b6538ac3109eb7b9a9a9a3d80d7cc93dd370b5a
Pavankumar Vijapur <quic_pvijapur@quicinc.com>
2023-03-07 18:50:05 +0530

    wakeup : Fix resume cause unknown print
    log_irq_wakeup_reason needs to be called to have
    the irq which woke up the device added to leaf_irq
    which can be printed later. This is fixed on newer
    kernel with commit id b19f0cc3fe48f73fdce9d4294c1c6a2b12d706eb
    
    Change-Id: I16245f94c94d647c6d93b538bd4e7ecbc93a5089
    Signed-off-by: Pavankumar Vijapur <quic_pvijapur@quicinc.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -22,6 +22,7 @@
 #include <linux/irq.h>
 #include <linux/interrupt.h>
 #include <linux/irqdesc.h>
+#include <linux/wakeup_reason.h>
 
 #include "power.h"
 
@@ -991,6 +992,7 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 		}
 		pm_wakeup_irq = irq_number;
 		pm_system_wakeup();
+		log_irq_wakeup_reason(irq_number);
 	}
 }
 
