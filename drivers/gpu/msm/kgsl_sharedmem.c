aaf7adb24429563b26768a18ba49e3141a9d14b5
Kaushal Sanadhya <quic_ksanadhy@quicinc.com>
2023-11-29 22:55:10 +0530

    msm: kgsl: Do not free sharedmem if it cannot be unmapped
    If sharedmem cannot be unmapped from the mmu, it can still be accessed
    by the GPU. Therefore it is not safe to free the backing memory. In the
    case that unmap fails, do not free it or return it to the system.
    
    Change-Id: Iad3e86d043f129a4d71cf862865d9033d4a315e3
    Signed-off-by: Lynus Vaz <quic_lvaz@quicinc.com>
    Signed-off-by: Kaushal Sanadhya <quic_ksanadhy@quicinc.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2002,2007-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -533,6 +533,9 @@ int kgsl_unlock_sgt(struct sg_table *sgt)
 
 static void kgsl_page_alloc_free(struct kgsl_memdesc *memdesc)
 {
+	if (memdesc->priv & KGSL_MEMDESC_MAPPED)
+		return;
+
 	kgsl_page_alloc_unmap_kernel(memdesc);
 	/* we certainly do not expect the hostptr to still be mapped */
 	BUG_ON(memdesc->hostptr);
@@ -622,6 +625,9 @@ static int kgsl_contiguous_vmfault(struct kgsl_memdesc *memdesc,
 static void kgsl_cma_coherent_free(struct kgsl_memdesc *memdesc)
 {
 	unsigned long attrs = 0;
+
+	if (memdesc->priv & KGSL_MEMDESC_MAPPED)
+		return;
 
 	if (memdesc->hostptr) {
 		if (memdesc->priv & KGSL_MEMDESC_SECURE) {
