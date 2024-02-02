CodeLinaro-mirror/la_platform_vendor_opensource_dataipa

dataipa: ipa3: Adding support for EoGRE stats
Made changes to query and display EoGRE Uplink and Downlink
stats from uC.

Change-Id: I454c81da970dc37792a84067a897761427f0b5a1
Signed-off-by: Srinivas Sai Mannam <quic_srinmann@quicinc.com>

drivers/platform/msm/ipa/ipa_v3/ipa_uc_offload_i.h
@@ -47,7 +47,7 @@
 #define FEATURE_ENUM_VAL(feature, opcode) ((feature << 5) | opcode)
 #define EXTRACT_UC_FEATURE(value) (value >> 5)
 
-#define IPA_HW_NUM_FEATURES 0x8
+#define IPA_HW_NUM_FEATURES 0x9
 
 /**
  * enum ipa3_hw_features - Values that represent the features supported
@@ -69,6 +69,8 @@ enum ipa3_hw_features {
 	IPA_HW_FEATURE_ZIP		=	0x4,
 	IPA_HW_FEATURE_NTN		=	0x5,
 	IPA_HW_FEATURE_OFFLOAD		=	0x6,
+	IPA_HW_FEATURE_UNIT		=	0x7,
+	IPA_HW_FEATURE_EOGRE		=	0x8,
 	IPA_HW_FEATURE_MAX		=	IPA_HW_NUM_FEATURES
 };
 

arter97/caf_msm-4.14

drivers/platform/msm/ipa/ipa_v3/ipa_uc_offload_i.h
@@ -51,7 +51,7 @@
 #define FEATURE_ENUM_VAL(feature, opcode) ((feature << 5) | opcode)
 #define EXTRACT_UC_FEATURE(value) (value >> 5)
 
-#define IPA_HW_NUM_FEATURES 0x8
+#define IPA_HW_NUM_FEATURES 0xC
 
 /**
  * enum ipa3_hw_features - Values that represent the features supported
@@ -73,6 +73,8 @@ enum ipa3_hw_features {
 	IPA_HW_FEATURE_ZIP		=	0x4,
 	IPA_HW_FEATURE_NTN		=	0x5,
 	IPA_HW_FEATURE_OFFLOAD		=	0x6,
+	IPA_HW_FEATURE_UNIT		=	0x7,
+	IPA_HW_FEATURE_EOGRE		=	0xB,
 	IPA_HW_FEATURE_MAX		=	IPA_HW_NUM_FEATURES
 };
 